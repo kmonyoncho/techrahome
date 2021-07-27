36824771c5aeac5b028795bd5cb3450902f39413
kmonyoncho <kepha.manwa@outlook.com>
2021-07-27 12:45:27 +0300

    4

--- src/Components/Products/Product/Product.js
+++ src/Components/Products/Product/Product.js
@@ -8,8 +8,11 @@ import {
   IconButton,
 } from "@material-ui/core";
 import { AddShoppingCart } from "@material-ui/icons";
+import useStyles from "./styles";
 
-const Product = () => {
+const Product = ({ product }) => {
+  const classes = useStyles();
+
   return (
     <Card className={classes.root}>
       <CardMedia
@@ -24,7 +27,15 @@ const Product = () => {
           </Typography>
           <Typography variant="h5">{product.price}</Typography>
         </div>
+        <Typography variant="h2" color="textSecondary">
+          {product.description}
+        </Typography>
       </CardContent>
+      <CardActions disableSpacing className={classes.cardActions}>
+        <IconButton aria-label="Add to Cart">
+          <AddShoppingCart />
+        </IconButton>
+      </CardActions>
     </Card>
   );
 };
