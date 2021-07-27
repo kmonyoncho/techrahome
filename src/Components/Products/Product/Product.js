be90c2b0c7dafe9eb53403ec44f9ee2941c7f254
kmonyoncho <kepha.manwa@outlook.com>
2021-07-27 12:24:04 +0300

    product component

---
+++ src/Components/Products/Product/Product.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
+
+function Product() {
+  return <div></div>;
+}
+
+export default Product;
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
+import { product } from "prelude-ls";
 
-const Products = [
+const products = [
   { id: 1, name: "Shoes", Description: "Running Shoes" },
   { id: 2, name: "Macbook", Description: "Apple Macbook Pro" },
   {
@@ -17,7 +18,13 @@ const Products = [
 ];
 const Products = () => {
   <main>
-    <Grid container justify="center" spacing={4}></Grid>
+    <Grid container justify="center" spacing={4}>
+      {products.map((products) => (
+        <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+          <Product />
+        </Grid>
+      ))}
+    </Grid>
   </main>;
 };
 
